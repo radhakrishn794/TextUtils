7a5e4a8f0f71ec1cf5d33d296d2e3dae9ef01371
Abhinav yadav <yadavabhi17104@gmail.com>
2023-03-19 19:46:13 +0530

    textarea changed

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -20,9 +20,15 @@ export default function Textarea(props) {
     props.showAlert("text cleared","success");
   }
   const replaceWord = () => {
+    let occur= prompt("If want to replace all occurence type 1 \n or if only first occurence type 0 ");
     let old = prompt("Enter the word you want to replace");
     let newWord = prompt("Enter the new word");
-    setText(text.replace(old, newWord));
+    if(occur==='1')
+    {setText(text.replaceAll(old, newWord));}
+    else if(occur=== '0')
+    {setText(text.replace(old, newWord));}
+    else
+    {alert("Enter either 0 or 1");}
     props.showAlert("word replaced","success");
   }
   const handleChange = (event) => {
